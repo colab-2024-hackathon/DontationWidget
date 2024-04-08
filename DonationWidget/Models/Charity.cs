ac50ff6753848c254661e9c28d1609a2d3e01013
jhartman <jhartman@ecu.com>
2024-04-08 14:34:41 -0500

    Add charity donation target dropdown

---
+++ DonationWidget/Models/Charity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonationWidget.Models
+{
+    public class Charity
+    {
+        public Guid AccountIdentifier { get; set; }
+        public string Name { get; set; }
+        public string CharityInfo { get; set; }
+        public decimal TotalDonationAmount { get; set; }
+        public decimal GoalAmount { get; set; }
+        public string WebsiteUrl { get; set; }
+        public string LogoUrl { get; set; }
+    }
+}
